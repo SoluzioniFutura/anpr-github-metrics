a5161fa2271c03963416890411f810cf152a5553
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-10-08 01:24:08 +0200

    fix repositories

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -2,27 +2,18 @@ import React from "react"
 
 import Repository from "./Repository"
 
-const gh = new GitHub()
-
-class Repositories extends Component {
-
-  componentDidMount() {
-    gh.getUser(user).listRepos()
-      .then(response => {
-        console.log(response)
-        this.setState({"repos": response.data})
-      })
-  }
-
-  render() {
-    if (this.state && this.state.repos) {
-      return (
-        <ul>
-          {this.state.repos.map(repo => <li key={repo.name} className={'repo'}><Repository data={repo}/></li>)}
-        </ul>
+const Repositories = props =>
+  <ul>
+    {
+      props.repos.map(repo =>
+        <li key={ repo.name }>
+          <Repository
+            data={ repo }
+            user={ props.user }
+          />
+        </li>
       )
     }
-  }
-}
+  </ul>
 
 export default Repositories
