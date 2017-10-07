4816cd45b869a2dcf48b687f14d544657a9054a0
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-10-07 18:33:46 +0200

    elenco repositories + singola repository con nuemro issues

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,6 +1,13 @@
 import React from "react"
 
 const Repository = props =>
-  <p>{props.data.name}</p>
+  <p>
+    <span style={{"marginRight": "20px"}}>
+      { props.data.name }
+    </span>
+    {
+      <span>{"Open issues: "}{ props.data.open_issues_count }</span>
+    }
+  </p>
 
 export default Repository
