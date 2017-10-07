57ac54516eadc34fbd6146ab3e389f5b02542937
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-10-07 16:25:02 +0200

    test utilizzo sdk github

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { Component } from 'react'
-import './App.css'
+import React from "react"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
-}
+import Repositories from "./Repositories"
+
+const App = () =>
+  <div>
+    <Repositories />
+  </div>
+
 
 export default App
---
+++ src/Repositories.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+
+import GitHub from "github-api"
+
+const gh = new GitHub()
+
+class Repositories extends Component {
+
+  componentDidMount() {
+    gh.getUser("vforvalerio87").listRepos()
+      .then(response => {
+        this.setState({"repos": response.data})
+      })
+  }
+
+  render() {
+    if (this.state && this.state.repos) {
+      console.log(this.state.repos)
+      return this.state.repos.map(repo => <p key={repo.name}>{repo.name}</p>)
+    } else {
+      return <p>{"Loading..."}</p>
+    }
+  }
+}
+
+export default Repositories
