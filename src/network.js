e71a93594685b4e7f1886cc1a4bf8983bf7309f9
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-10-07 17:07:05 +0200

    merge

---
+++ src/network.js
@@ -0,0 +1,17 @@
+import GitHub from "github-api";
+const gh = new GitHub();
+
+const user = "expressjs";
+export const getRepos = () => new Promise((resolve, reject) => {
+  gh.listRepos(user)
+    .then(resolve)
+    .then(reject)
+});
+
+
+
+export const test = () => new Promise((resolve, reject) => {
+  resolve('test')
+});
+
+console.log('test');
