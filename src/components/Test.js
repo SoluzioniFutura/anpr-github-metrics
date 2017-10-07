1006080c3f254f1b78c2bb872d887198f05b01f0
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-10-07 19:55:05 +0200

    mods

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import * as network from "../network";
+import * as network from "../api";
 
 import { user, repo } from "./../config"
 
@@ -7,7 +7,6 @@ class Test extends Component {
   render() {
     // CODICE QUI //
     network.getIssues(user, repo)
-      .then(network.getAvgIssueClosingTime)
       .then(console.log)
       .catch(console.error)
     // FINE CODICE //
