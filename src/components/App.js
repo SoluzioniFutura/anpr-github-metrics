4cba316e3dc3bbd2ed94f7245a6f738552789ba2
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-10-08 11:27:05 +0200

    rimosso componente test

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,10 @@
 import React from "react"
 
 import Organization from "./Organization"
-import Test from "./Test"
 
 import { user } from "./../config"
 
 const App = () =>
-  [
-    <Organization key={"app-organization"} name={user} />,
-    <Test key={"app-test"} />
-  ]
+  <Organization name={user} />
 
 export default App
