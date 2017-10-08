d7f661e243d4f239147b2e57019be12d121eb46a
Valerio Versace <valerio.versace@soluzionifutura.it>
2017-10-08 02:09:06 +0200

    nick merda

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -21,27 +21,15 @@ class Repository extends Component {
   render() {
     return(
       <div>
-<<<<<<< HEAD
       <a key = { this.props.name } onClick = { this.handleClick } className = { 'panel-block is-active columns is-mobile' }>
         <span className = { 'column is-4' }>{ this.props.data.name }</span>
         <span className = { 'column is-offset-4' } style = {{ float: 'right' }}>Open issues: { this.props.data.open_issues_count }</span>
       </a>
-=======
-        <p
-          style={{"cursor": "pointer"}}
-          onClick={this.handleClick}
-        >
-          <span style={{"marginRight": "20px"}}>
-            { this.props.data.name }
-          </span>
-          <span>{"Open issues: "}{ this.props.data.open_issues_count }</span>
-        </p>
->>>>>>> f4f9f7335195e8099846c660503b6080a73df24d
-        <RepositoryData
-          user = { this.props.user }
-          name = { this.props.data.name }
-          isActive = { this.state.isActive }
-        />
+      <RepositoryData
+        user = { this.props.user }
+        name = { this.props.data.name }
+        isActive = { this.state.isActive }
+      />
       </div>
     )
   }
